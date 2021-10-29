f6039022aa940ef8ed8784d794344ddccdd29192
BlueSparrow <bluesparrowtoken@gmail.com>
2021-10-29 16:21:01 +0330

    The last version of BlueSparrow Contract

---
+++ VRF/VRFConsumerBase.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./VRFRequestIDBase.sol";
+import "./SafeMath.sol";
+import "./LinkTokenInterface.sol";
+
+abstract contract VRFConsumerBase is VRFRequestIDBase {
+
+  using SafeMath for uint256;
+
+  
+  function fulfillRandomness(bytes32 requestId, uint256 randomness)
+    internal virtual;
+
+  
+  uint256 constant private USER_SEED_PLACEHOLDER = 0;
+
+  
+  function requestRandomness(bytes32 _keyHash, uint256 _fee)
+    internal returns (bytes32 requestId)
+  {
+    LINK.transferAndCall(vrfCoordinator, _fee, abi.encode(_keyHash, USER_SEED_PLACEHOLDER));
+    // This is the seed passed to VRFCoordinator. The oracle will mix this with
+    // the hash of the block containing this request to obtain the seed/input
+    // which is finally passed to the VRF cryptographic machinery.
+    uint256 vRFSeed  = makeVRFInputSeed(_keyHash, USER_SEED_PLACEHOLDER, address(this), nonces[_keyHash]);
+    // nonces[_keyHash] must stay in sync with
+    // VRFCoordinator.nonces[_keyHash][this], which was incremented by the above
+    // successful LINK.transferAndCall (in VRFCoordinator.randomnessRequest).
+    // This provides protection against the user repeating their input seed,
+    // which would result in a predictable/duplicate output, if multiple such
+    // requests appeared in the same block.
+    nonces[_keyHash] = nonces[_keyHash].add(1);
+    return makeRequestId(_keyHash, vRFSeed);
+  }
+
+  LinkTokenInterface immutable internal LINK;
+  address immutable private vrfCoordinator;
+
+  // Nonces for each VRF key from which randomness has been requested.
+  //
+  // Must stay in sync with VRFCoordinator[_keyHash][this]
+  mapping(bytes32 /* keyHash */ => uint256 /* nonce */) private nonces;
+
+  /**
+   * @param _vrfCoordinator address of VRFCoordinator contract
+   * @param _link address of LINK token contract
+   *
+   * @dev https://docs.chain.link/docs/link-token-contracts
+   */
+  constructor(address _vrfCoordinator, address _link)  {
+    vrfCoordinator = _vrfCoordinator;
+    LINK = LinkTokenInterface(_link);
+  }
+
+  // rawFulfillRandomness is called by VRFCoordinator when it receives a valid VRF
+  // proof. rawFulfillRandomness then calls fulfillRandomness, after validating
+  // the origin of the call
+  function rawFulfillRandomness(bytes32 requestId, uint256 randomness) external {
+    require(msg.sender == vrfCoordinator, "Only VRFCoordinator can fulfill");
+    fulfillRandomness(requestId, randomness);
+  }
+}
